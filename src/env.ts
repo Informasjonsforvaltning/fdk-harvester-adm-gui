bf83d8c44abed620e60a13dcc17a2734edfff5dd
Gjermund Skobba <gjermund@skobba.net>
2019-10-25 14:15:56 +0200

    fix: set default backend to dev to docker when run on host

--- src/env.ts
+++ src/env.ts
@@ -9,7 +9,7 @@ interface EnvironmentVariables {
 const env = ((window as any).env || {
   OIDC_ISSUER: 'http://localhost:8084/auth/realms/fdk',
   OIDC_CLIENT_ID: 'fdk-admin-gui',
-  FDK_HARVEST_ADMIN_HOST: 'http://localhost:8135'
+  FDK_HARVEST_ADMIN_HOST: 'http://localhost:8134'
 }) as EnvironmentVariables;
 
 export default { ...env } as Configuration;
