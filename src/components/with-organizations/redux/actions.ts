d329b0c28dc11aab6f94d7b20254337b78cef369
NilsOveTen <github.nils.ove@tendenes.com>
2021-08-27 10:35:14 +0200

    fix: fetch org if not available in org-cat

--- src/components/with-organizations/redux/actions.ts
+++ src/components/with-organizations/redux/actions.ts
@@ -1,7 +1,10 @@
 import {
   FETCH_ORGANIZATIONS_FAILED,
   FETCH_ORGANIZATIONS_REQUESTED,
-  FETCH_ORGANIZATIONS_SUCCEEDED
+  FETCH_ORGANIZATIONS_SUCCEEDED,
+  FETCH_ORGANIZATION_FAILED,
+  FETCH_ORGANIZATION_REQUESTED,
+  FETCH_ORGANIZATION_SUCCEEDED
 } from './action-types';
 
 import type { Organization } from '../../../types';
@@ -25,6 +28,31 @@ export function fetchOrganizationsSucceeded(organizations: Organization[]) {
 export function fetchOrganizationsFailed(message: string) {
   return {
     type: FETCH_ORGANIZATIONS_FAILED,
+    payload: {
+      message
+    }
+  };
+}
+
+export function fetchOrganizationRequested(id: string) {
+  return {
+    type: FETCH_ORGANIZATION_REQUESTED,
+    payload: { id }
+  };
+}
+
+export function fetchOrganizationSucceeded(organization: Organization) {
+  return {
+    type: FETCH_ORGANIZATION_SUCCEEDED,
+    payload: {
+      organization
+    }
+  };
+}
+
+export function fetchOrganizationFailed(message: string) {
+  return {
+    type: FETCH_ORGANIZATION_FAILED,
     payload: {
       message
     }
